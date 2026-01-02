210930f74e7b423db49f12aff16bd7aff2d6f720
Dani Esquilas <desquilas@gmail.com>
2026-01-02 15:19:37 +0100

    feat: add stillward logo and favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Stillward') }}</title>
+
+        <!-- Favicon -->
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
