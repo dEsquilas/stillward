f3f03354f346af1e10f584c5f5883ef25b69e43c
Dani Esquilas <desquilas@gmail.com>
2026-01-03 14:03:13 +0100

    feat: add pwa meta tags and mobile viewport config

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,12 +2,23 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover">
 
         <title inertia>{{ config('app.name', 'Stillward') }}</title>
 
+        <!-- PWA Meta Tags -->
+        <meta name="theme-color" content="#030712">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+        <meta name="apple-mobile-web-app-title" content="Stillward">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="application-name" content="Stillward">
+        <meta name="description" content="Track your goals and resolutions">
+
         <!-- Favicon -->
         <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+        <link rel="apple-touch-icon" href="/icons/icon.svg">
+        <link rel="mask-icon" href="/icons/icon.svg" color="#8B5CF6">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
