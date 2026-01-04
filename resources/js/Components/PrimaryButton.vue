781cce37c217adf57fb19d1cd2395fa6d095cc7b
Dani Esquilas <desquilas@gmail.com>
2026-01-04 12:50:35 +0100

    chore: remove unused PrimaryButton component

--- resources/js/Components/PrimaryButton.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <button
-        class="inline-flex items-center rounded-md border border-transparent bg-white px-4 py-2 text-xs font-semibold uppercase tracking-widest text-gray-900 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-200"
-    >
-        <slot />
-    </button>
-</template>
